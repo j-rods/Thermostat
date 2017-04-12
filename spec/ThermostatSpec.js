d27702ceca7c1c18a030f926df04db2361629c41
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-12 10:58:44 +0100

    increase thermostat degrees by a number

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,7 +7,14 @@ describe('thermostat', function() {
 
   describe('degrees', function() {
     it('starts at 20', function() {
-      expect(thermostat.degrees()).toEqual(20);
+      expect(thermostat.degrees).toEqual(20);
+    });
+  });
+
+  describe('increases temperature', function() {
+    it('by 3', function() {
+      thermostat.increase(3)
+      expect(thermostat.degrees).toBe(23);
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,11 @@
 function Thermostat() {
-
+  this.degrees = 20
 }
 
 Thermostat.prototype.degrees = function() {
   return 20
+}
+
+Thermostat.prototype.increase = function(degree) {
+  this.degrees = this.degrees + degree
 }
