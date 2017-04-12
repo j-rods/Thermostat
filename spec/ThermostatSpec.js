062cd4c507e53288f332d4c6a03d1a0bc84bb1c4
Edward Ward <edwardwardward@gmail.com>
2017-04-12 15:37:28 +0100

    html base

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('thermostat', function() {
   var thermostat;
 
@@ -12,32 +14,39 @@ describe('thermostat', function() {
   });
 
   describe('increases temperature', function() {
-    it('by 3', function() {
-      thermostat.up(3)
-      expect(thermostat.degrees).toBe(23);
+    it('by 1', function() {
+      thermostat.up()
+      expect(thermostat.degrees).toBe(21);
     });
 
     it('throws error if above 25 and powerSavingMode is on', function() {
       expect(function() {
-        thermostat.up(6)
+        for(var i = 0; i < 6; i++) {
+        thermostat.up()
+      }
       }).toThrowError(TypeError, "can't go above 25")
     });
     it('throws error if above 32 and powerSavingMode if off', function(){
       thermostat.changeMode()
       expect(function() {
-        thermostat.up(13)
+        for(var i = 0; i < 13; i++) {
+        thermostat.up()
+      }
       }).toThrowError(TypeError, "can't go above 32")
     });
   });
 
   describe('decreases temperature', function() {
     it('by 3', function() {
-      thermostat.down(3)
+      thermostat.down()
+      thermostat.down()
+      thermostat.down()
       expect(thermostat.degrees).toBe(17);
     });
     it(":minimum degrees is 10", function() {
-      thermostat.down(10)
-      expect(function() {
+      for(var i = 0; i < 10; i++) {
+      thermostat.down()
+    }      expect(function() {
         thermostat.down(1)
       }).toThrowError(TypeError, "can't go below 10")
     });
@@ -52,7 +61,8 @@ describe('thermostat', function() {
 
   describe('reset temperature', function() {
     it("to 20", function() {
-      thermostat.up(2)
+      thermostat.up()
+      thermostat.up()
       thermostat.resetTemp()
       expect(thermostat.degrees).toBe(20);
     });
@@ -61,6 +71,19 @@ describe('thermostat', function() {
   describe('energy usage', function() {
     it('default set to medium-usage', function() {
       expect(thermostat.energyUsage()).toBe("medium-usage")
+    });
+    it('set to low-usage', function(){
+      for(var i = 0; i < 4; i++) {
+      thermostat.down()
+      }
+      expect(thermostat.energyUsage()).toBe("low-usage")
+    });
+    it('set to high-usage', function(){
+      thermostat.changeMode()
+      for(var i = 0; i < 6; i++) {
+      thermostat.up()
+    }
+      expect(thermostat.energyUsage()).toBe("high-usage")
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function Thermostat() {
   this.degrees = 20
   this.powerSavingMode = true
@@ -7,24 +9,24 @@ Thermostat.prototype.degrees = function() {
   return 20
 }
 
-Thermostat.prototype.up = function(degree) {
+Thermostat.prototype.up = function() {
   if(this.powerSavingMode === true) {
-    if(this.degrees + degree > 25) {
+    if(this.degrees + 1 > 25) {
       throw new TypeError("can't go above 25")
     }
   } else if(this.powerSavingMode === false) {
-    if(this.degrees + degree > 32) {
+    if(this.degrees + 1 > 32) {
       throw new TypeError("can't go above 32")
     }
   }
-    this.degrees = this.degrees + degree
+    this.degrees += 1
 }
 
 Thermostat.prototype.down = function(degree) {
-  if(this.degrees - degree < 10 ) {
+  if(this.degrees - 1 < 10 ) {
     throw new TypeError("can't go below 10")
   } else {
-  this.degrees = this.degrees - degree
+  this.degrees -= 1
   }
 }
 
@@ -37,7 +39,11 @@ Thermostat.prototype.resetTemp = function() {
 }
 
 Thermostat.prototype.energyUsage = function() {
-  if(this.degrees > 18 && this.degrees < 25) {
+  if(this.degrees >= 18 && this.degrees < 25) {
     return "medium-usage"
+  } else if(this.degrees < 18) {
+    return "low-usage"
+  } else {
+    return "high-usage"
   }
 }
